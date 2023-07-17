a6774668d4cc9f84f578198d66685620cc1f128c
Rafał Toborek <kontakt@toborek.info>
2023-07-17 12:24:22 +0200

    Added GitHub Actions integration

---
+++ rector.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/src',
+    ]);
+
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        LevelSetList::UP_TO_PHP_81,
+    ]);
+};
